28b8e2d788b61117b81791adad4ebdcfd88abdaa
OlesYudin <oles.yudin2001@gmail.com>
2022-01-22 15:58:55 +0200

    Remove some comments

--- Lesson_3-VPC/security-group.tf
+++ Lesson_3-VPC/security-group.tf
@@ -34,16 +34,3 @@ resource "aws_security_group" "sg" {
     Environment = var.env
   }
 }
-
-
-
-
-# dynamic "ingress" {
-#   for_each = ["80", "443", "8080"]
-#   content {
-#     from_port   = ingress.value
-#     to_port     = ingress.value
-#     protocol    = "tcp"
-#     cidr_blocks = ["0.0.0.0/0"]
-#   }
-# }
